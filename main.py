951d9dd8d5216db59f7f8d8da0c6eddc0d34d7e6
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2020-02-15 14:25:53 -0500

    added automatic audio playback

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import sys
 from gtts import gTTS
+import playsound
+import os
+
+
 def main():
   print(sys.version)
   
@@ -7,6 +11,14 @@ def txt2speech(filename):
   file = open(filename, "r")
   tts = gTTS(file.read())
   tts.save('hello.mp3')
+  cwd = os.getcwd()
+  print(cwd)
+
+  playsound.playsound("hello.mp3", True)
+  
+  #p = vlc.MediaPlayer("file:///" + cwd + "hello.mp3")
+  #p.play() 
+
 
 
 if __name__== "__main__":
