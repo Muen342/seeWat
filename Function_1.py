5ee854f953d7bc2ba8c7d4cf06b26482565399b6
Gursimmer <46874155+Gurs-Ban@users.noreply.github.com>
2020-02-15 15:07:24 -0500

    Add files via upload

---
+++ Function_1.py
@@ -0,0 +1,24 @@
+import os, io
+from tkinter import filedialog, Tk
+from google.cloud import vision
+from google.cloud .vision import types
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
+
+FileName = "Img.jpg"
+FolderPath = r"C:\Users\gursi\Documents\seeWat\VisionAPISeeWat"
+FullPath = os.path.join(FolderPath,FileName)
+
+def GetText(FilePath):
+	client = vision.ImageAnnotatorClient()
+
+
+	with io.open(FilePath,'rb') as image:
+
+		content = image.read()
+
+	image = vision.types.Image(content=content)
+	response = client.document_text_detection(image=image)
+	return (response.full_text_annotation.text)
+
+print(GetText(FullPath))
