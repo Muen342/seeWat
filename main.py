f9a0d8d83bad27f8d1b5aff0e06f7f422a7903ee
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2020-02-15 14:32:37 -0500

    deleted extra whitespace

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 from gtts import gTTS
 import playsound
-import os
-
 
 def main():
   print(sys.version)
@@ -11,15 +9,7 @@ def txt2speech(filename):
   file = open(filename, "r")
   tts = gTTS(file.read())
   tts.save('hello.mp3')
-  cwd = os.getcwd()
-  print(cwd)
-
   playsound.playsound("hello.mp3", True)
-  
-  #p = vlc.MediaPlayer("file:///" + cwd + "hello.mp3")
-  #p.play() 
-
-
 
 if __name__== "__main__":
   main()
