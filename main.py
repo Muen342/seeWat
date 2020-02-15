5f35b616a4ba1fa8592573317a3d4bdf1779a807
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2020-02-15 13:03:59 -0500

    added txt2speech function

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import sys
+from gtts import gTTS
 def main():
-    print("Hello World!")
+  print(sys.version)
+  
+def txt2speech(filename):
+  file = open(filename, "r")
+  tts = gTTS(file.read())
+  tts.save('hello.mp3')
 
-if __name__ == "__main__":
-    main()
+
+if __name__== "__main__":
+  main()
+
+txt2speech("demofile.txt")
+
+
